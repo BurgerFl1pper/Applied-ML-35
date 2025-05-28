245a0d2f54cca7de63de7ffb31718413b4237f8a
hihina17 <h.naruse@student.rug.nl>
2025-05-28 23:53:31 +0200

    LSTM model initial draft

--- project_name/models/LSTM.py
+++ project_name/models/LSTM.py
@@ -1,9 +1,19 @@
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import pad_sequences
-import pandas as pd
 import os
 import re
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import classification_report
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
+from tensorflow.keras.metrics import AUC, Precision, Recall
+
+max_words = 10000
+max_len = 200
+embedding_dim = 100
 
 def loadData():
     """
@@ -66,3 +76,44 @@ def tokenizingData(X_train, X_test, y_train, y_test):
 
     return X_train_pad, X_test_pad, y_train_binary, y_test_binary, tokenizer, mlb
 
+def lstm_model(num_labels):
+    inputs = Input(shape=(max_len,))
+    x = Embedding(input_dim=max_words, output_dim=embedding_dim, input_length=max_len)(inputs)
+    x = LSTM(128, return_sequences=False)(x)
+    x = Dense(64, activation='relu')(x)
+    outputs = Dense(num_labels, activation='sigmoid')(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def predict(X_train_pad, X_test_pad, y_train_binary, y_test_binary, mlb):
+    model = lstm_model(num_labels=len(mlb.classes_))
+    model.compile(loss='binary_crossentropy', optimizer='adam',  metrics=[
+        Precision(name="precision"),
+        Recall(name="recall")])
+
+    model.fit(X_train_pad, y_train_binary, 
+              epochs=10, 
+              batch_size=64, 
+              validation_split=0.2)
+
+    y_pred_prob = model.predict(X_test_pad)
+    y_pred = (y_pred_prob > 0.5).astype(int)
+    return y_pred
+    
+   
+def main():
+    finished_data = loadData()
+    X_train, X_test, y_train, y_test = splitData(finished_data)
+    
+    X_train = cleanData(X_train)
+    X_test = cleanData(X_test)
+    
+    X_train_pad, X_test_pad, y_train_binary, y_test_binary, tokenizer, mlb = tokenizingData(X_train, X_test, y_train, y_test)
+    
+    y_pred = predict(X_train_pad, X_test_pad, y_train_binary, y_test_binary, mlb)
+    
+    print(classification_report(y_test_binary, y_pred, target_names=mlb.classes_))
+
+
+main()
